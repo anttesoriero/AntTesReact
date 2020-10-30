a6b058b7a4e0d1b585385ccd91fa91bf93c71dd1
Anthony Tesoriero <anttesoriero@gmail.com>
2020-10-29 23:06:36 -0400

    Footer Changes

--- AntTes/react-typescript/src/App.tsx
+++ AntTes/react-typescript/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Button, Container } from 'reactstrap';
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import LandingPage from './Pages/LandingPage';
+import ErrorPage from './Pages/ErrorPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact={true} path="/" render={(props) => <LandingPage />} />
+        <Route exact={true} path="/error" render={(props) => <ErrorPage />} />
         {/* 
           <Route exact={true} path="/survey" render={(props) => <SurveyPage />} />
           <Route exact={true} path="/tasks" render={(props) => <TaskPage />} />
