ef6e979dfd4c6871b71c74b72ad061bc984e9e22
Anthony Tesoriero <anttesoriero@gmail.com>
2021-03-10 08:09:01 -0500

    Add Oddjobs redirect

--- AntTes/react-typescript/src/App.tsx
+++ AntTes/react-typescript/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import LandingPage from './Pages/LandingPage';
 import ErrorPage from './Pages/ErrorPage';
 import Splash from './Pages/Splash';
+import Oddjobs from './Redirect/Oddjobs';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact={true} path="/" render={(props) => <LandingPage />} />
         <Route exact={true} path="/error" render={(props) => <ErrorPage />} />
         <Route exact={true} path="/splash" render={(props) => <Splash />} />
+        <Route exact={true} path="/oddjobs" render={(props) => <Oddjobs />} />
         <Redirect to="/error" />
       </Switch>
     </BrowserRouter>
