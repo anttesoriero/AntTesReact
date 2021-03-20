1249c3b689363b544f5062ad67e4c8dce53fae1f
Anthony Tesoriero <anttesoriero@gmail.com>
2021-03-20 13:17:19 -0400

    Nav Mobile

--- AntTes/react-typescript/src/Components/Navigation.tsx
+++ AntTes/react-typescript/src/Components/Navigation.tsx
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown,
-    DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+    DropdownToggle, DropdownMenu, DropdownItem, UncontrolledCollapse, ListGroup, ListGroupItem } from 'reactstrap';
 import { withRouter } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import {GiHamburgerMenu} from 'react-icons/gi'
 {/*import { MdPerson } from 'react-icons/md';
 import { IconContext } from 'react-icons'; */}
 
@@ -12,44 +13,67 @@ const Navigation = (prop: {pageName: string}) => {
 
     const isMobile = window.innerWidth < 1000;
 
+    // const toggleNav = () => {
+    //     setIsOpen(!isOpen);
+    //     console.log(isOpen)
+    // };
+
     return (
         <div>
             <Helmet>
                 <title>Anthony Tesoriero | {prop.pageName}</title>
             </Helmet>
-            <Navbar color="dark" dark expand="md"  className="navstyle">
-                <NavbarBrand href="/" className={'mx-auto brand'} style={{fontSize: 30}}>Anthony Tesoriero</NavbarBrand>
-                <NavbarToggler onClick={toggleNav} />
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav navbar className="mr-auto">
-                        {/*
-                        <NavItem>
-                            <NavLink href="/sample">Sample</NavLink>
-                        </NavItem>
-                        */}
-                    </Nav>
-                    
 
-                    {/* Right side of nav bar */}
-                    <UncontrolledDropdown navbar inNavbar>
-                        <DropdownToggle nav caret style={{color: 'white', fontWeight: 'bolder'}}>
-                            MENU
-                        </DropdownToggle>
-                        {/* Checks if signed in, then conditionally renders different menu */}
-                        <DropdownMenu right>
-                                <DropdownItem href="./#resume">
-                                    R&eacute;sum&eacute;
-                                </DropdownItem>
+            {isMobile?
+                <div>
+                    <Navbar color="dark" dark expand="md"  className="navstyle">
+                    <NavbarBrand href="/" className={'mx-auto brand'} style={{fontSize: 30}}>Anthony Tesoriero</NavbarBrand>
+                        <GiHamburgerMenu style={{ color: 'white' }} id="toggler" />
+                    </Navbar>
 
-                                <DropdownItem divider />
-                                
-                                <DropdownItem href="http://anttes.com/AnthonyTesorieroResume.pdf" target="_blank">
-                                    Full R&eacute;sum&eacute; PDF
-                                </DropdownItem>
-                            </DropdownMenu>
-                    </UncontrolledDropdown>
-                </Collapse>
-            </Navbar>
+                    {/* Dropdown */}
+                    <UncontrolledCollapse toggler="#toggler">
+                        <ListGroup flush>
+                            <ListGroupItem tag="a" href="./#resume" className="centered" action><h5 style={{ fontWeight: 'bold' }}>R&eacute;sum&eacute;</h5></ListGroupItem>
+                            <ListGroupItem tag="a" href="http://anttes.com/AnthonyTesorieroResume.pdf" target="_blank" className="centered" action><h5 style={{ fontWeight: 'bold' }}>Full R&eacute;sum&eacute; PDF</h5></ListGroupItem>
+                        </ListGroup>
+                    </UncontrolledCollapse>
+                </div>
+            :
+                <Navbar color="dark" dark expand="md"  className="navstyle">
+                    <NavbarBrand href="/" className={'mx-auto brand'} style={{fontSize: 30}}>Anthony Tesoriero</NavbarBrand>
+                    <NavbarToggler onClick={toggleNav} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav navbar className="mr-auto">
+                            {/*
+                            <NavItem>
+                                <NavLink href="/sample">Sample</NavLink>
+                            </NavItem>
+                            */}
+                        </Nav>
+                        
+
+                        {/* Right side of nav bar */}
+                        <UncontrolledDropdown navbar inNavbar>
+                            <DropdownToggle nav caret style={{color: 'white', fontWeight: 'bolder'}}>
+                                MENU
+                            </DropdownToggle>
+                            {/* Checks if signed in, then conditionally renders different menu */}
+                            <DropdownMenu right>
+                                    <DropdownItem href="./#resume">
+                                        R&eacute;sum&eacute;
+                                    </DropdownItem>
+
+                                    <DropdownItem divider />
+                                    
+                                    <DropdownItem href="http://anttes.com/AnthonyTesorieroResume.pdf" target="_blank">
+                                        Full R&eacute;sum&eacute; PDF
+                                    </DropdownItem>
+                                </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Collapse>
+                </Navbar>
+            }
         </div>
     );
 }
