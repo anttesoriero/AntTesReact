2a5de1a8c39222763a843ff96eac9cfe454b39a9
Anthony Tesoriero <anttesoriero@gmail.com>
2022-05-22 17:19:46 -0400

    Splash Update

--- AntTes/main-site-anttes/src/Pages/Contact.tsx
+++ AntTes/main-site-anttes/src/Pages/Contact.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Navigation from '../Components/Navigation';
-import {Container} from 'reactstrap';
+import { Container } from 'reactstrap';
 import Footer from '../Components/Footer';
 
 const Contact = () => {
 
     return (
         <div className="App">
-            <Navigation pageName="Contact"/>
-            
+            <Navigation pageName="Contact" />
+
             <Container>
                 <h1 className="centered">Coming Soon</h1>
 
             </Container>
             <br />
-            <Footer/>
+            <Footer />
         </div>
     );
 }
